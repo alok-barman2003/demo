9d40f245be34057b20d41abb49731ed0f0dbf871
Alok Barman <alok.barman@indigi.co.in>
2026-09-09 10:15:53 +0000

    rest

--- shop/srv/service.cds
+++ shop/srv/service.cds
@@ -1,3 +1,12 @@
+@protocol: 'rest'
+
 service product {
     function printhelloworld(input:String) returns String;
+
+    function addition(num1:Integer, num2:Integer) returns Integer;
+
+    @open
+    type object{};
+    function myfunction(category: Integer) returns object;
+    
 }
--- shop/srv/service.js
+++ shop/srv/service.js
@@ -7,5 +7,33 @@ module.exports = cds.service.impl(async function (srv) {
         console.log(req.data.input)//print the request config from server
         return `${req.data.input} World`  
     })
+
+    srv.on('addition', req => {
+        console.log(req.data)
+        let result= req.data.num1 + req.data.num2;
+        return result
+    })
+
+    srv.on('myfunction', req => {
+
+        let result= {}
+
+        if (req.data.category == 1){
+            result.product= 'BMW',
+            result.price= '1200 USD',
+            result.location= 'New Delhi'
+        } else {
+            result.product= 'Audi',
+            result.stock= 120,
+            result.priceArray= {
+                "Price": 47457,
+                "Discount": '20%'
+            }
+        }
+
+        console.log(result)
+        return result;
+
+    })
     
 })
