b582b83a2becdc90f9d447e16f0554618c9fc28d
Alok Barman <alok.barman@indigi.co.in>
2026-09-09 07:31:37 +0000

    hello world

---
+++ shop/srv/service.js
@@ -0,0 +1,11 @@
+const cds= require('@sap/cds');
+const { stringify } =require('querystring');
+
+module.exports = cds.service.impl(async function (srv) {
+
+    srv.on('printhelloworld', req => {
+        console.log(req.data.input)//print the request config from server
+        return `${req.data.input} World`  
+    })
+    
+})
